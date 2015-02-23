8365f2638f5fb516672c6115eff5cd1070efc491
Sam Blackshear <sam.blackshear@gmail.com>
2015-02-23 09:43:00 -0700

    Adding some extra lifecycle methods to constants

--- src/main/scala/edu/colorado/droidel/constants/AndroidLifecycle.scala
+++ src/main/scala/edu/colorado/droidel/constants/AndroidLifecycle.scala
@@ -13,11 +13,16 @@ object AndroidLifecycle {
   
   // lifecycle callbacks written in WALA style
   val ACTIVITY_ONCREATE = "onCreate(Landroid/os/Bundle;)V"
+  val ACTIVITY_ONATTACHFRAGMENT = "onAttachFragment(Landroid/app/Fragment;)V"
+  val ACTIVITY_ONCONTENTCHANGED = "onContentChanged()V"
   val ACTIVITY_ONSTART = "onStart()V"
   val ACTIVITY_ONRESTOREINSTANCESTATE = "onRestoreInstanceState(Landroid/os/Bundle;)V"
   val ACTIVITY_ONPOSTCREATE = "onPostCreate(Landroid/os/Bundle;)V"
   val ACTIVITY_ONRESUME = "onResume()V"
   val ACTIVITY_ONPOSTRESUME = "onPostResume()V"
+  val ACTIVITY_ONATTACHEDTOWINDOW = "onAttachedToWindow()V"
+  val ACTIVITY_ONCREATEOPTIONSMENU = "onCreateOptionsMenu(Landroid/view/Menu;)B"
+  val ACTIVITY_ONPREPAREOPTIONSMENU = "onPrepareOptionsMenu(Landroid/view/Menu;)B"
   val ACTIVITY_ONCREATEDESCRIPTION = "onCreateDescription()Ljava/lang/CharSequence;"
   val ACTIVITY_ONSAVEINSTANCESTATE = "onSaveInstanceState(Landroid/os/Bundle;)V"
   val ACTIVITY_ONPAUSE = "onPause()V"
@@ -41,8 +46,16 @@ object AndroidLifecycle {
   val APPLICATION_ONTERMINATE = "onTerminate()V"
     
   // TODO: this is not complete
+  val FRAGMENT_ONATTACH = "onAttach(Landroid/app/Activity;)V"
+  val FRAGMENT_ONCREATEVIEW = "onCreateView(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;"
+  val FRAGMENT_ONVIEWCREATED = "onViewCreated(Landroid/view/View;Landroid/os/Bundle;)V"
   val FRAGMENT_ONCREATE = "onCreate(Landroid/os/Bundle;)V"
   val FRAGMENT_ONSTART = "onStart()V"
+  val FRAGMENT_ONRESUME = "onResume()V"
+  val FRAGMENT_ONPAUSE = "onPause()V"
+  val FRAGMENT_ONSTOP = "onStop()V"
+  val FRAGMENT_ONDESTROYVIEW = "onDestroyView()V"
+  val FRAGMENT_ONDETACH = "onDetach()V"
 
   // TODO: what are these?
   val APPLIFECYCLECALLBACK_ONACTIVITYSTARTED = "void onActivityStarted(android.app.Activity)";
@@ -55,12 +68,16 @@ object AndroidLifecycle {
   
   // mapping of framework-created lifecycle types to callbacks defined on those types
   val frameworkCbMap = Map(
-    ACTIVITY_TYPE -> List(ACTIVITY_ONCREATE, 
+    ACTIVITY_TYPE -> List(ACTIVITY_ONCREATE,
+                          ACTIVITY_ONATTACHFRAGMENT,
+                          ACTIVITY_ONCONTENTCHANGED,
                           ACTIVITY_ONSTART, 
                           ACTIVITY_ONRESTOREINSTANCESTATE,
                           ACTIVITY_ONPOSTCREATE,
                           ACTIVITY_ONRESUME,
                           ACTIVITY_ONPOSTRESUME,
+                          ACTIVITY_ONATTACHEDTOWINDOW,
+                          ACTIVITY_ONCREATEOPTIONSMENU,
                           ACTIVITY_ONCREATEDESCRIPTION,
                           ACTIVITY_ONSAVEINSTANCESTATE,
                           ACTIVITY_ONPAUSE,
@@ -81,11 +98,24 @@ object AndroidLifecycle {
     APPLICATION_TYPE -> List(APPLICATION_ONCREATE,
                              APPLICATION_ONTERMINATE
                         ),
-    FRAGMENT_TYPE -> List(FRAGMENT_ONCREATE, 
-                          FRAGMENT_ONSTART
+    FRAGMENT_TYPE -> List(FRAGMENT_ONATTACH,
+                          FRAGMENT_ONCREATE,
+                          FRAGMENT_ONCREATEVIEW,
+                          FRAGMENT_ONSTART,
+                          FRAGMENT_ONRESUME,
+                          FRAGMENT_ONPAUSE,
+                          FRAGMENT_ONSTOP,
+                          FRAGMENT_ONDETACH
                      ),
-    APP_FRAGMENT_TYPE -> List(FRAGMENT_ONCREATE, 
-                              FRAGMENT_ONSTART)
+    APP_FRAGMENT_TYPE -> List(FRAGMENT_ONATTACH,
+                              FRAGMENT_ONCREATE,
+                              FRAGMENT_ONCREATEVIEW,
+                              FRAGMENT_ONSTART,
+                              FRAGMENT_ONRESUME,
+                              FRAGMENT_ONPAUSE,
+                              FRAGMENT_ONSTOP,
+                              FRAGMENT_ONDETACH
+                         )
   )
 
   // can't use a lazy val because we need the class hierarchy to construct
