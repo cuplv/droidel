03bb0c0a6cca3c4bf8c0d2c211f57c53e84f60c4
Shawn Meier <shawn.meier@gmail.com>
2016-02-01 09:28:57 -0500

    progress on classes dir selection

--- src/main/scala/edu/colorado/droidel/driver/FileLocationHeuristics.scala
+++ src/main/scala/edu/colorado/droidel/driver/FileLocationHeuristics.scala
@@ -29,21 +29,39 @@ object FileLocationHeuristics {
    */
   def getAllAppPackages(appPathf: File): Set[String] = {
 
-    val filt: Array[File] = getRecursiveListOfFilesFilt(appPathf, ".*\\.java".r)
+    val filt: Array[File] = getRecursiveListOfFilesFilt(appPathf, ".*\\.java$".r)
     val pkgRegex = "^package.*".r
+    //assumption that a package should be greater than 3 chars
     val pkgs: Set[String] = filt.flatMap((file: File) => {
-        val lines: Iterator[String] = Source.fromFile(file).getLines()
-        lines.flatMap((line: String) => pkgRegex.findFirstIn(line))
-    }).toSet.filter((a: String) => a.length > 2) //assumption that a package should be greater than 3 chars
+        if(file.exists()) {
+          val lines: Iterator[String] = Source.fromFile(file).getLines()
+          lines.flatMap((line: String) => pkgRegex.findFirstIn(line))
+        }else None
+    }).toSet.filter((a: String) => a.length > 2)
 
-
-    ???
+    pkgs
   }
   def getBinPath(appPath: String): String = {
     val appPathf: File = new File(appPath)
-    getRecursiveListOfFilesFilt(appPathf, ".*\\.class".r)
-    getAllAppPackages(appPathf)
+    val classFiles: Array[File] = getRecursiveListOfFilesFilt(appPathf, ".*\\.class".r)
+    val packages: Set[String] = getAllAppPackages(appPathf)
+    val substrdr: Set[String] = packages.map((a:String) => a.split("\\.")
+      .mkString(File.separator)
+      .substring(8, a.length -1))
+    val classFilesFullPaths = classFiles.map((a: File) => a.getAbsolutePath)
 
+    //find directory which contains all class files compiled
+    val map: Set[Array[String]] = substrdr.map((a:String)=> classFilesFullPaths.flatMap((b: String) => {
+      val contains: Boolean = b.contains(a)
+      if(contains){
+        val str: String = b.substring(0, b.indexOfSlice(a))
+        Some(str)
+      } else None
+    }))
+    val containingDirs: Set[String] = map.flatMap((a: Array[String]) =>{
+      val toSet: Set[String] = a.toSet
+      toSet
+    })
 
 
 
